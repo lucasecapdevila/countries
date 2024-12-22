ff1f7434e6dd1929dc3685190970ba05d2b7be57
Lucas Capdevila <lcapdevila60@gmail.com>
2024-12-22 19:22:07 -0300

    borders of a country added

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ const PORT = 3000
 
 
 // Start the server - Route to fetch available countries
-app.get('/https://date.nager.at/api/v3/AvailableCountries', async (req, res) => {
+app.get('/api/countries', async (req, res) => {
   try {
     const response = await axios.get('https://date.nager.at/api/v3/AvailableCountries')
     res.json(response.data)
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error')
+    res.status(500).send('Error fetching available countries:', err.response?.data || err.message)
+  }
+})
+
+
+// Route to fetch country info
+app.get('/api/borders/:countryCode', async (req, res) => {
+  const { countryCode } = req.params
+  try {
+    const countryResponse = await axios.get(`https://date.nager.at/api/v3/CountryInfo/${countryCode}`)
+    const country = countryResponse.data;
+    res.json({ country: {code: country.code, name: country.name, borders: country.borders} })
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(err.response?.data || err.message)
   }
 })
 
